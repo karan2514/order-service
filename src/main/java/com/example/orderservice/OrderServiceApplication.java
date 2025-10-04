035d38d219feeb39ffa2e2edfc357c740b198481
karan2514 <karan2514singh@gmail.com>
2025-10-04 11:40:16 +0530

    added war changes

--- src/main/java/com/example/orderservice/OrderServiceApplication.java
+++ src/main/java/com/example/orderservice/OrderServiceApplication.java
@@ -2,10 +2,17 @@ package com.example.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class OrderServiceApplication {
+public class OrderServiceApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(OrderServiceApplication.class);
     }
 }
